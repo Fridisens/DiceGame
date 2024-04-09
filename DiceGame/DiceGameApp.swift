3b624840a70c26477936ffc28ee4238a47072109
Fridisens <fridisens@gmail.com>
2024-04-09 15:28:32 +0200

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Frida Dahlqvist on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
