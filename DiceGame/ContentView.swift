1a7c2cd18be0b55fdca4962e107fe0d48a87da2a
Fridisens <fridisens@gmail.com>
2024-04-19 10:07:50 +0200

    Files to github

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -8,16 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var diceNumber1 = 1
+    @State var diceNumber2 = 2
+    
+    @State var sum = 0
+
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 255/256, green: 102/256, blue: 178/256)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text ("\(sum)")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Spacer()
+                
+                HStack {
+                    DiceView(n: diceNumber2)
+                    DiceView(n: diceNumber1)
+                    
+                }.onAppear(){
+                    newDiceValue()
+                }
+                
+                Button(action: {
+                    rollDice()
+                }, label: {
+                    Text ("Roll")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                    
+                })
+                
+                .background(Color.black)
+                .cornerRadius(15.0)
+                Spacer()
+                
+                
+                Button(action: {
+                    resetDices()
+                }, label: {
+                    Text("Reset")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                })
+                .background(Color.black)
+                .cornerRadius(15.0)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func newDiceValue() {
+        diceNumber1 = Int.random(in: 1...6)
+        diceNumber2 = Int.random(in: 1...6)
+        
+    }
+    
+    func rollDice(){
+        newDiceValue()
+        sum += diceNumber1 + diceNumber2
+        
+    }
+    
+    func resetDices(){
+        sum = 0
     }
 }
+
+struct DiceView : View {
+    let n: Int
+    var body: some View {
+        Image(systemName: "die.face.\(n).fill")
+            .resizable()
+            .aspectRatio(contentMode:.fit)
+            .padding()
+    }
+}
+
 
 #Preview {
     ContentView()
